d2f59041f29cd036bee62640c4e4c90a73a9baea
陈海俊 <353376352@qq.com>
2022-06-07 18:28:39 +0800

    add commander

--- packages/core/lib/core.js
+++ packages/core/lib/core.js
@@ -1,9 +1,23 @@
 #! /usr/bin/env node
 
-let pkg = require('../package.json')
+let pkg = require('../package.json');
+let commander = require('commander');
+// let program = commander.program;
+let program = new commander.Command();
 
-checkVersion();
+let dev = program.command('dev [port]');
+dev.action((port) => {
+    console.log('dev start on ', port)
+})
 
+program
+    .version(pkg.version)
+    // .option('-f, --foo <foo>', '测试')
+    .parse(process.argv)
+
+// checkVersion();
+
+// console.log(program.opts().foo)
 
 function checkVersion() {
     console.log(pkg.version)
