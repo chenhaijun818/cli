83dd0b6c504af34766d348b405615fbf6f5deb94
陈海俊 <353376352@qq.com>
2022-06-08 18:27:57 +0800

    add command: init

--- core/cli/index.js
+++ core/cli/index.js
@@ -1,5 +1,28 @@
 #! /usr/bin/env node
 
+const {Command} = require('commander')
+const pkg = require('./package.json')
+const init = require('@lizen/init')
+
+const program = new Command();
+
+registerCommand();
+
 function registerCommand() {
-    console.log('register command')
+    program
+        .name(Object.keys(pkg.bin)[0])
+        .usage('<command> [options]')
+        .version(pkg.version);
+
+    program.command('init [name]').option('-f, --force', '是否强制初始化').action(init);
+
+    program.on('option:debug', () => {
+        console.log('on debug')
+    });
+
+    program.on('command:*', (e) => {
+        console.log('unknown command:', e.shift())
+    });
+
+    program.parse(process.argv)
 }
