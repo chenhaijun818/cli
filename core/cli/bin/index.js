24dd90453ca62c972744935c83106969aefc86b7
陈海俊 <www.353376352@qq.com>
2022-11-22 05:35:30 +0800

    process unknown command

--- core/cli/bin/index.js
+++ core/cli/bin/index.js
@@ -1,5 +1,29 @@
 #! /usr/bin/env node
 
-const cli = require('../lib/cli');
+const {Command} = require('commander')
 
-cli();
+const pkg = require('../package.json')
+const npmlog = require('@lizen/log')
+
+const program = new Command()
+
+program
+    .name('lizen')
+    .usage('<command> [options]')
+    .version(pkg.version)
+    .option('-d, --debug', '是否启用调试模式', false);
+
+program.command('create <name> <location>')
+    .action((name, location) => {
+        console.log(name, location)
+    });
+
+program.on('command:*', () => {
+    npmlog.info('未知的命令');
+    program.outputHelp()
+})
+
+program.parse();
+// const cli = require('../lib/cli');
+
+// cli();
